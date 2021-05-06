ce07db8d65946bbe842e16fa562ffd87450780ee
ZacharyFolk <zacharyfolk@gmail.com>
2021-05-06 13:44:32 -0700

    Add custom fonts

--- wp-content/themes/goya-child/functions.php
+++ wp-content/themes/goya-child/functions.php
@@ -12,3 +12,23 @@ wp_enqueue_style( 'goya-style' , get_template_directory_uri() . '/style.css' );
 }
 
 add_action(  'wp_enqueue_scripts', 'goya_child_enqueue_styles', 99 );
+
+add_filter( 'goya_main_font_choices', 'goya_main_font_custom' );
+function goya_main_font_custom() {
+ return array(
+  'fonts' => array(
+   'google'  => array( 'popularity', 700 ),
+   'families' => array(
+    'custom' => array(
+     'text'   => 'Goya Custom Fonts',
+     'children' => array(
+      array( 'id' => 'Linden Hill', 'text' => 'Linden Hill' ),
+     ),
+    ),
+   ),
+   'variants' => array(
+    'Linden Hill' => array( 'regular','italic','bold','bolditalic','400','400italic','600','600italic','800','800italic' ),
+   ),
+  ),
+ );
+}
