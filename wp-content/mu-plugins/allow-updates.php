c254e1d6762438c522ef3509c9d97a42e8460d26
ZacharyFolk <zacharyfolk@gmail.com>
2022-03-24 22:09:27 -0700

    :recycle: Leftovers and removing some bad css

--- wp-content/mu-plugins/allow-updates.php
+++ wp-content/mu-plugins/allow-updates.php
@@ -1,10 +1,10 @@
-<?php
-/*
-  Plugin Name: Allow Updates
-  Description: Override to allow automatic updates under versioned control
-  Version: 0.1
-  Author: Zac
-  Author URI:
-*/
-
-add_filter( 'automatic_updates_is_vcs_checkout', '__return_false', 1 );
+<?php
+/*
+  Plugin Name: Allow Updates
+  Description: Override to allow automatic updates under versioned control
+  Version: 0.1
+  Author: Zac
+  Author URI:
+*/
+
+add_filter( 'automatic_updates_is_vcs_checkout', '__return_false', 1 );
